25de5a820a89edeae5af705944a82df7a4902511
erfanoabdi <erfangplus@gmail.com>
2017-07-22 09:50:24 +0430

    ApplyPatch: cleanup
    Signed-off-by: erfanoabdi <erfangplus@gmail.com>

--- ApplyPatch.cpp
+++ ApplyPatch.cpp
@@ -20,7 +20,6 @@
 
 // <file> <target> <tgt_sha1> <size> <init_sha1> <patch>
 int ApplyPatchFn(const char* name, State* state, int argc, char * argv[]) {
-    printf("hey\n");
     char* source_filename = argv[1];
     char* target_filename = argv[2];
     char* target_sha1 = argv[3];
@@ -39,18 +38,10 @@ int ApplyPatchFn(const char* name, State* state, int argc, char * argv[]) {
     
     int patchcount = (argc-4) / 2;
     
-    //std::vector<std::unique_ptr<Value, decltype(&FreeValue)>> patch_shas;
-    //std::vector<std::unique_ptr<Value, decltype(&FreeValue)>> patches;
-    
     std::vector<char*> patch_sha_str;
     std::vector<Value*> patch_ptrs;
     
-    // Protect values by unique_ptrs first to get rid of memory leak.
     for (int i = 0; i < patchcount * 2; i += 2) {
-        //patch_shas.emplace_back(StringValue(strdup(argv[i])), FreeValue);
-        
-        printf("i=%i patchcount=%i argc=%i argv[i]=%s argv[i+1]=%s\n",i,patchcount,argc,argv[i+5],argv[i+6]);
-        
         Value patch_value;
         patch_value.type = VAL_BLOB;
         
@@ -69,30 +60,11 @@ int ApplyPatchFn(const char* name, State* state, int argc, char * argv[]) {
         fclose (rm);
         
         patch_value.size = length;
-        //patches.emplace_back(patch_value, FreeValue);
         
         patch_sha_str.push_back(argv[i+5]);
         patch_ptrs.push_back(&patch_value);
     }
     
-    /*for (int i = 0; i < patchcount; ++i) {
-        if (patch_shas[i]->type != VAL_STRING) {
-            printf("%s(): sha-1 #%d is not string", name, i);
-            return kArgsParsingFailure;
-        }
-        if (patches[i]->type != VAL_BLOB) {
-            printf("%s(): patch #%d is not blob", name, i);
-            return kArgsParsingFailure;
-        }
-    }
-    
-    std::vector<char*> patch_sha_str;
-    std::vector<Value*> patch_ptrs;
-    for (int i = 0; i < patchcount; ++i) {
-        patch_sha_str.push_back(patch_shas[i]->data);
-        patch_ptrs.push_back(patches[i].get());
-    }*/
-    printf("applypatch\n");
     int result = applypatch(source_filename, target_filename,
                             target_sha1, target_size,
                             patchcount, patch_sha_str.data(), patch_ptrs.data(), NULL);
